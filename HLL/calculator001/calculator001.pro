383d88867af3fa12efefeec71b1d46d8514ef23b
qyvlik <qyvlik@qq.com>
2016-05-30 16:55:43 +0800

    添加中缀表达式转后缀表达式，并且进行进行计算的例子
    添加中缀表达式转后缀表达式，并且进行进行计算的例子

---
+++ HLL/calculator001/calculator001.pro
@@ -0,0 +1,13 @@
+QT += core
+QT -= gui
+
+TARGET = calculator001
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
+
+CONFIG += C++11
+
---
+++ HLL/calculator001/main.cpp
@@ -0,0 +1,259 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <assert.h>
+
+using namespace std;
+
+// std::stoi
+
+
+//class Node {
+//public:
+//    enum NodeType {
+//        ExprNode,
+//        TermNode,
+//        FactorNode,
+//        NumberLiteral,
+//    };
+//};
+
+class lexerStream
+{
+public:
+    lexerStream(const vector<string> l):
+        l(l),
+        index(0)
+    {}
+
+    void next() {
+        index < l.size()-1 ? index ++ : 0;
+    }
+
+    void rockBack() {
+        index > 0 ? index -- : 0;
+    }
+
+    string current() const {
+        return l.at(index);
+    }
+
+private:
+    vector<string> l;
+    size_t index;
+};
+
+// expr -> expr + term
+//       | expr - term
+//       | term
+
+// term -> term * factor
+//       | term / factor
+//       | factor
+
+// factor -> ( expr )
+//         | number
+//         | id
+
+
+// expr     -> term trest
+
+// trest    -> + term
+//           | - term
+//           | ε
+
+// term     -> factor frest
+
+// frest    -> * factor
+//           | / factor
+//           | ε
+
+// factor   -> NUMBER
+//           | ( expr )
+
+class Calculator {
+public:
+    Calculator(const vector<string>& code):
+        code(code)
+    { }
+
+    // op 后必定有两个操作数
+
+    bool isOperator(const string& s) {
+        return s == "+" || s == "-" || s == "*" || s == "/";
+    }
+
+    string result() {
+
+        string current;
+
+        do {
+            current = code.front();
+            code.erase(code.begin());
+
+            if(!isOperator(current)) {
+                calc.push(current);
+            } else {
+
+                string arg0 = calc.top();
+                calc.pop();
+                string arg1 = calc.top();
+                calc.pop();
+
+                calc.push(calcula(current, arg0, arg1));
+            }
+        } while(!code.empty());
+
+        return calc.top();
+    }
+
+    string calcula(const string& operation, const string& arg0, const string& arg1) {
+
+        if(operation == "+") {
+
+            return add(arg0, arg1);
+        } else if(operation == "-") {
+
+            return sub(arg0, arg1);
+        } else if(operation == "*") {
+
+            return mul(arg0, arg1);
+        } else if(operation == "/") {
+
+            return div(arg0, arg1);
+        } else {
+            assert(0);
+            return string("0");
+        }
+    }
+
+    string add(const string& arg0, const string& arg1) {
+        int a0 = std::stoi(arg0);
+        int a1 = std::stoi(arg1);
+        return std::to_string(a0 + a1);
+    }
+
+    string sub(const string& arg0, const string& arg1) {
+        int a0 = std::stoi(arg0);
+        int a1 = std::stoi(arg1);
+        return std::to_string(a0 - a1);
+    }
+
+    string mul(const string& arg0, const string& arg1) {
+        int a0 = std::stoi(arg0);
+        int a1 = std::stoi(arg1);
+        return std::to_string(a0 * a1);
+    }
+
+    string div(const string& arg0, const string& arg1) {
+        int a0 = std::stoi(arg0);
+        int a1 = std::stoi(arg1);
+
+        assert(a1 != 0);
+
+        return std::to_string(a0 / a1);
+    }
+
+    vector<string> code;
+    stack<string> calc;
+};
+
+
+class Parser {
+public:
+    void expr(lexerStream* lexers)
+    {
+        term(lexers);
+        while(lexers->current() == "+" || lexers->current() == "-") {
+
+            string op = lexers->current();
+
+            lexers->next();
+            term(lexers);
+
+            cout << op;     // PUSH
+            code.push_back(op);
+        }
+    }
+
+    void term(lexerStream* lexers)
+    {
+        factor(lexers);
+
+        while(lexers->current() == "*" || lexers->current() == "/") {
+
+            string op = lexers->current();
+
+            lexers->next();
+            factor(lexers);
+
+            cout << op;     // PUSH
+            code.push_back(op);
+        }
+    }
+
+    void factor(lexerStream* lexers)
+    {
+        string current = lexers->current();
+
+        if(current == "(") {
+            lexers->next();
+
+            expr(lexers);
+            lexers->next();
+
+            string e = lexers->current();
+            if(e != ")") {
+                cout << "lost `(" << endl;
+            }
+        } else {
+            cout << current;      // PUSH
+
+            code.push_back(current);
+
+            lexers->next();
+        }
+    }
+
+    vector<string> code;
+};
+
+
+int main()
+{
+
+    // 1 + 1
+    //    vector<string> l;
+    //    l.push_back("1");
+    //    l.push_back("+");
+    //    l.push_back("1");
+
+    // 2 * ( 1 + 1 )
+    vector<string> l;
+    l.push_back("2");
+    l.push_back("*");
+    l.push_back("(");
+    l.push_back("1");
+    l.push_back("+");
+    l.push_back("1");
+    l.push_back(")");
+
+
+    lexerStream stream(l);
+
+    Parser parser;
+    parser.expr(&stream);
+
+//    cout << endl;
+//    cout << parser.code.front();
+
+     Calculator calculator(parser.code);
+     cout << endl;
+     cout <<
+             calculator.result();
+
+
+
+}
